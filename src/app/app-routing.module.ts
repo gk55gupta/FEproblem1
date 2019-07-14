b733a4cee928cfad50e50e6e8e4e26f961829074
Gopal <gk55gupta@gmail.com>
2019-07-15 02:34:35 +0530

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ResultComponent } from './result/result.component';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'home', component: MainComponent },
   { path: 'result', component: ResultComponent }
 ];
 
